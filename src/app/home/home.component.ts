1b4ae2a4d8ade26074dd03487db6fc3ad28e5f2c
Logan <loganshalom@gmail.com>
2024-03-23 16:31:36 -0300

    implement random background img

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+  @ViewChild('main')
+  main!: ElementRef;
 
+  imageUrls: string[] = [
+    'url(/assets/desktop-background/1.png)',
+    'url(/assets/desktop-background/2.png)',
+    'url(/assets/desktop-background/3.png)',
+    'url(/assets/desktop-background/4.png)',
+    'url(/assets/desktop-background/5.png)',
+    'url(/assets/desktop-background/6.png)',
+  ];
+
+  ngAfterViewInit() {
+    this.changeImg();
+  }
+
+  changeImg() {
+    let index = Math.floor((Math.random() * 10) % this.imageUrls.length);
+    this.main.nativeElement.style.backgroundImage = this.imageUrls[index];
+
+    console.log(index);
+  }
 }
